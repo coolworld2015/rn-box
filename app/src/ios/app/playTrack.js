c8050d305b1ab37a3dc3a868c71a80dbc82bd0d5
coolworld2015 <wintermute2015@ukr.net>
2019-09-13 21:43:00 +0300

    [ios] - added AppStore v.1.02

--- app/src/ios/app/playTrack.js
+++ app/src/ios/app/playTrack.js
@@ -87,7 +87,7 @@ const styles = StyleSheet.create({
     container: {
         flex: 1,
         justifyContent: 'center',
-        backgroundColor: 'black'
+        backgroundColor: 'white'
     },
     backgroundVideo: {
         position: 'absolute',
